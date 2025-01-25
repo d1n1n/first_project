809fb92269c5edecb89805555c9404445e1da672
d1n1n <kmbox89@gmail.com>
2025-01-25 01:28:36 -0500

    first commit

---
+++ first_project/urls.py
@@ -0,0 +1,10 @@
+
+from django.contrib import admin
+from django.urls import path
+from products import views
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('home/', views.home), 
+    path('gallery/', views.gallery), 
+    
+]
